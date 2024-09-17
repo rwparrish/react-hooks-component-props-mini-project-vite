5a6004d9bead1a8430f797e9690edf093fe01428
rwparrish <60946750+rwparrish@users.noreply.github.com>
2024-09-17 08:52:12 -0600

    update package.json file

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   test: {
+//     environment: 'jsdom',
+//     globals: true,
+//     setupFiles: './src/__tests__/setup.js',
+//   }
+// })
+
