3b0d6ca0a9bc6beecaad3dd9de0b0ba1996d74b6
rwparrish <60946750+rwparrish@users.noreply.github.com>
2024-09-19 08:35:12 -0600

    something

--- src/__tests__/Setup.js
+++ src/__tests__/Setup.js
@@ -1,7 +1,7 @@
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
-import 'whatwg-fetch'; // this is for any CodeGrade fetch errors
+// import 'whatwg-fetch'; // this is for any CodeGrade fetch errors
 import {Blob} from 'node:buffer'; // this is for CodeGrade running node 16
 
 afterEach(() => {
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
-
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [react()],
-//   test: {
-//     environment: 'jsdom',
-//     globals: true,
-//     setupFiles: './src/__tests__/setup.js',
-//   }
 // })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/__tests__/Setup.js',
+  }
+})
 
